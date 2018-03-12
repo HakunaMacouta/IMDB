8a472ce47d43af52b4fefc6dd7c5150121b84509
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-03-12 09:58:38 +0100

    add webpack + scripts

--- src/main.js
+++ src/main.js
@@ -5,20 +5,11 @@
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.   Thomas Blanc
  * ----------------------------------------------------------------------------
-<<<<<<< HEAD
  */
 import Vue from 'vue' //librairie "vue" dans node_modules
 import app from './app.vue' //fichier app.vue local
-import VueRouter from 'vue-router'
-import Item from './components/movieitem.vue'
-
-Vue.use(VueRouter);
-Vue.component('movie-item', Item);
 
 new Vue({
 	el: '#app',
 	render: h => h(app)
 });
-
-Vue.config.devtools = true;
-
