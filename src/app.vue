fc39925da00a2ca78fd745cdafa8f6bb2c1c1485
Thomas Blanc <thomas.hakunamacouta.blanc@gmail.com>
2018-03-12 10:47:43 +0100

    add css

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,26 @@
 <template>
-	<div>
-		<span id="msg">{{message}}</span>
-	</div>
+	<table class="u-full-width">
+		<thead>
+		<tr>
+			<th>Name</th>
+			<th>Realease Date</th>
+			<th>Director</th>
+			<th>Synopsis</th>
+			<th>Edit</th>
+			<th>Delete</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr v-for="">
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><button class="button fa fa-pencil-alt" v-on:click="modify(movie)"></button></td>
+			<td><button class="button fa fa-trash-alt" v-on:click="remove(movie)"></button></td>
+		</tr>
+		</tbody>
+	</table>
 </template>
 
 <script>
